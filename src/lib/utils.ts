c3ab3ad4e24044d24d3c7a30a5763f96255e31d1
Håkon <hkleven6@outlook.com>
2026-02-20 15:05:24 +0100

    W

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function removeUndefinedValues<T extends Object>(obj: T): Partial<T> {
+  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v !== undefined)) as Partial<T>;
+}
--- src/server/ai/imageEdit.ts
+++ src/server/ai/imageEdit.ts
@@ -1,33 +1,53 @@
+import { removeUndefinedValues } from "@/lib/utils"
 import axios from "axios"
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
 const BFL_API_KEY = process.env.BFL_API_KEY
 
-function imageEditGoogle(prompt: string, imagesBase64: string[],  output: Partial<{
+if (!GOOGLE_API_KEY || !BFL_API_KEY) {
+    throw new Error("Missing API keys for Google or BFL")
+}
+
+export function imageEditGoogle(prompt: string, imagesBase64: string[],  output: Partial<{
     width: number
     height: number
     format: "png" | "jpeg"
 }>) {
     const MODEL = "gemeni-3-pro-image-preview" as const
 
-    return axios.post("/api/google/imageEdit", {
-        prompt,
-        images,
-        model
-    })
+
 }
 
 
-function imageEditBFL(prompt: string, imagesBase64: string[], output: Partial<{
+export async function imageEditBFL(prompt: string, imagesBase64: string[], output: Partial<{
     width: number
     height: number
     format: "png" | "jpeg"
-}>) {
+}> = {}) {
     const MODEL = "flux-2-klein-9b" as const
 
-    return axios.post("/api/bfl/imageEdit", {
+    if (imagesBase64.length === 0 || imagesBase64.length > 8) {
+        throw new Error("Must provide between 1 and 8 images")
+    }
+
+    const [ refrenceImage, ...additionalImages ] = imagesBase64
+
+    const body: Record<string, string | number> = removeUndefinedValues({
         prompt,
-        images,
-        model
+        input_image: refrenceImage,
+        width: output.width,
+        height: output.height,
+        format: output.format
+    })
+
+    for (const [i, image] of additionalImages.entries()) {
+        body[`input_image_${i + 2}`] = image
+    }
+
+    return axios.post(`https://api.bfl.ai/v1/${MODEL}`, body, {
+        headers: {
+            "x-key": BFL_API_KEY,
+            "Content-Type": "application/json"
+        }
     })
 }
